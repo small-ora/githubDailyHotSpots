18a57c7eada150c46f9cf05ab973ba844aa88688
nelson <nelson.cheng.zhao@outlook.com>
2025-01-16 21:19:22 +0800

    init project

---
+++ src/main/kotlin/win/nelson/githubDailyHotSpots/GithubDailyHotSpotsApplication.kt
@@ -0,0 +1,11 @@
+package win.nelson.githubDailyHotSpots
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GithubDailyHotSpotsApplication
+
+fun main(args: Array<String>) {
+	runApplication<GithubDailyHotSpotsApplication>(*args)
+}
---
+++ src/test/kotlin/win/nelson/githubDailyHotSpots/GithubDailyHotSpotsApplicationTests.kt
@@ -0,0 +1,13 @@
+package win.nelson.githubDailyHotSpots
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GithubDailyHotSpotsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

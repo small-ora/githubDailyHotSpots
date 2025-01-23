3f4eb21573e5a65fcd1fbeb6eca8aba17a0580bb
nelson <nelson.cheng.zhao@outlook.com>
2025-01-24 00:05:54 +0800

    修改代码，增加容错

--- src/main/kotlin/win/nelson/githubDailyHotSpots/process/TrendingProcess.kt
+++ src/main/kotlin/win/nelson/githubDailyHotSpots/process/TrendingProcess.kt
@@ -1,6 +1,7 @@
 package win.nelson.githubDailyHotSpots.process
 
 import cn.hutool.core.codec.Base64
+import cn.hutool.http.Header
 import cn.hutool.http.HttpUtil
 import com.alibaba.fastjson2.JSONObject
 import org.slf4j.LoggerFactory
@@ -32,23 +33,33 @@ class TrendingProcess(val llmSummarize: LlmSummarize) : PageProcessor {
             return
         }
         val projectDatas = mutableListOf<ProjectData>()
+        val token = System.getenv("GITHUB_TOKEN") ?: throw IllegalStateException("GITHUB_TOKEN environment variable is not set")
         for (item in all) {
             // 提取项目名称
             val strings = item.split("/").filter { it.isNotEmpty() }
             val owner = strings.first()
             val repo = strings.last()
-            //获取项目信息
-            val repositoryReadme = HttpUtil.get("https://api.github.com/repos/$owner/$repo/readme")
-            val readmeInfo = JSONObject.parse(repositoryReadme).to(RepositoryReadme::class.java)
-            if (readmeInfo == null){
-                logger.error("无法获取README文件信息")
-                return
+            var summarize = "查询信息或调用大模型失败！"
+            try {
+                //获取项目信息
+                val repositoryReadme = HttpUtil.createGet("https://api.github.com/repos/$owner/$repo/readme")
+                    .header(Header.AUTHORIZATION, "Bearer $token")
+                    .execute()
+                    .body()
+                logger.info("仓库返回信息：$repositoryReadme")
+                val readmeInfo = JSONObject.parse(repositoryReadme).to(RepositoryReadme::class.java)
+                if (readmeInfo == null){
+                    logger.error("无法获取README文件信息")
+                    return
+                }
+                logger.info("仓库信息: $repo")
+                // 解码Base64内容
+                val readmeContent = Base64.decodeStr(readmeInfo.content)
+                //调用大模型总结
+                summarize = llmSummarize.summarize(readmeContent)
+            }catch (e: Exception){
+                logger.error("调用大模型失败！", e)
             }
-            logger.info("仓库信息: $repo")
-            // 解码Base64内容
-            val readmeContent = Base64.decodeStr(readmeInfo.content)
-            //调用大模型总结
-            val summarize = llmSummarize.summarize(readmeContent)
             val projectData = ProjectData(repo, "https://github.com/$owner/$repo", summarize)
             projectDatas.add(projectData)
         }

ef358675553274a716d556f650d55b18166e232b
nelson <nelson.cheng.zhao@outlook.com>
2025-01-23 23:07:06 +0800

    修改编译的仓库配置

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement {
 	repositories {
 		maven { url = uri("https://maven.aliyun.com/repository/public") }
-		maven { url = uri("https://repo.spring.io/milestone") }
-		maven { url = uri("https://repo.spring.io/snapshot") }
+		maven { url = uri("https://maven.aliyun.com/repository/spring") }
+		maven { url = uri("https://repo.spring.io/release") }
 		gradlePluginPortal()
 	}
 }

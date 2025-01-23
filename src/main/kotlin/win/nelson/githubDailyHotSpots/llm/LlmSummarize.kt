55cd7ec0705419192e67899824d5af59d3af7373
nelson <nelson.cheng.zhao@outlook.com>
2025-01-24 00:19:06 +0800

    修改代码，增加容错

--- src/main/kotlin/win/nelson/githubDailyHotSpots/llm/LlmSummarize.kt
+++ src/main/kotlin/win/nelson/githubDailyHotSpots/llm/LlmSummarize.kt
@@ -16,7 +16,7 @@ class LlmSummarize(val llmConfig: LlmConfig) {
         val message = JSONArray()
         var msg: MutableMap<String, String> = HashMap()
         msg["role"] = "system"
-        msg["content"] = "我会发给你一份github项目的readme,你帮我用中文总结归纳这个项目,要详细描述这个项目是做什么,有什么特点,使用什么语言开发的。"
+        msg["content"] = "我会发给你一份github项目的readme,你帮我用中文总结归纳这个项目,要详细描述这个项目是做什么,有什么特点,使用什么语言开发的。总结的描述使用html的格式，方便阅读！"
         message.add(msg)
         msg = HashMap()
         msg["role"] = "user"
